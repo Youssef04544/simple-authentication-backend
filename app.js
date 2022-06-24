f8d38382367b474a78bef3fd7dfa2c39434b7b7a
Youssef04544 <youssefbenyedder04@gmail.com>
2022-06-24 09:23:12 +0100

    implement protected route and authenticate user via jwt verification before requests

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const express = require("express");
 const app = express();
 
 const Authrouter = require("./routes/auth");
+const ProtectedRouter = require("./routes/protected");
+const authenticateUser = require("./middleware/authenticate");
+
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/api/v1", Authrouter);
+app.use("/api/v1/protectedInfo", authenticateUser, ProtectedRouter);
 
 app.listen(PORT, async () => {
   try {
--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,1 +1,24 @@
 //check the JWT token
+const jwt = require("jsonwebtoken");
+const { StatusCodes } = require("http-status-codes");
+
+const authenticate = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(StatusCodes.UNAUTHORIZED).json({ msg: "Unauthorized" });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = await jwt.verify(token, process.env.JWT_SECRET);
+    //attach user to the protected route
+    req.user = {
+      userId: payload.userId,
+      name: payload.username,
+    };
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = authenticate;
